01aef87213fd19c69a439be3d459bc5dc9c01101
Aidan McBride <aidankmcbride@gmail.com>
2025-01-19 18:32:24 -0500

    update home button hover

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -10,8 +10,8 @@
 </script>	
 
 <template>
-    <div class='navbar absolute bg-black-400 w-full top-0 left-0 right-0' role='navigation'>
-        <a class='nav-card text-2xl !lg:text-4xl' @click='onClickHome' aria-label='homeLink' data-testid='home-link' title='home'>
+    <div class='navbar absolute bg-black w-full top-0 left-0 right-0' role='navigation'>
+        <a class='nav-card fixed top-2text-2xl !lg:text-4xl' @click='onClickHome' aria-label='homeLink' data-testid='home-link' title='home'>
             <i class='pi pi-home !text-2xl text-orange-400 !lg:text-4xl'></i>
         </a>
     </div>
@@ -24,10 +24,13 @@
     top: .5em;
     right: .5em;
     color: var(--orange-400);
+    border-radius: 10px;
+    padding: .25em;
     &:hover {
         cursor: pointer;
         -webkit-box-shadow: 0 0 15px hsla(160, 100%, 37%, 0.2);
         box-shadow: 0 0 15px hsla(160, 100%, 37%, 0.2);
+        background: hsla(160, 100%, 37%, 0.2);
     }
     &:active {
         color: var(--orange-200);
